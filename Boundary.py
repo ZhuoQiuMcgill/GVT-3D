0ad6dfba98ec4bc17a0b435f7052e42286a98be8
Zhuo Qiu <QIUZHUOWORK@gmail.com>
2025-06-02 14:59:48 -0400

    REFORMAT PROJECT

--- Boundary.py
+++
@@ -1,9 +0,0 @@
-class Boundary:
-    def __init__(self, vertices, faces):
-        self.vertices = vertices
-        self.faces = faces
-        self.edges = []
-
-    def init_mesh(self):
-        pass
-
